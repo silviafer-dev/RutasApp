172fbc214ef2fe5430951144be316aefbd0999db
Silvia Ferrai <silviaferdeveloper@gmail.com>
2023-03-20 18:24:15 +0100

    Add permission map request in permissionsScreen

--- src/navigator/navigator.tsx
+++ src/navigator/navigator.tsx
@@ -8,14 +8,15 @@ const Stack = createStackNavigator();
 export const Navigator = () => {
   return (
     <Stack.Navigator
+      // initialRouteName="PermissionsScreen"
       screenOptions={{
         headerShown: false,
         cardStyle: {
           backgroundColor: 'white',
         },
       }}>
-      <Stack.Screen name="MapScreen" component={MapScreen} />
       <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+      <Stack.Screen name="MapScreen" component={MapScreen} />
     </Stack.Navigator>
   );
 };
