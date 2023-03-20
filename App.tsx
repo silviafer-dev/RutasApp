bd037a6628d57353ea62669d18f2ba6d8e82fad0
Silvia Ferrai <silviaferdeveloper@gmail.com>
2023-03-20 11:43:25 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const App = () => {
+  return (
+    <View>
+      <Text> Hola Mundo!</Text>
+      <Icon name="star-outline" size={30} color="red" />
+    </View>
+  );
+};
+
+export default App;
