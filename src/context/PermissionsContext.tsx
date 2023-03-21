7c785260cb09faa0c2912190814743fcc1af1af7
Silvia Ferrai <silviaferdeveloper@gmail.com>
2023-03-21 12:12:56 +0100

    Use useContext to get the permissions status

--- src/context/PermissionsContext.tsx
+++ src/context/PermissionsContext.tsx
@@ -1,5 +1,10 @@
 import React, { createContext, useState } from 'react';
-import { PermissionStatus } from 'react-native-permissions';
+import {
+  PERMISSIONS,
+  PermissionStatus,
+  request,
+} from 'react-native-permissions';
+import { Platform } from 'react-native';
 
 export interface PermissionState {
   locationStatus: PermissionStatus;
@@ -14,13 +19,25 @@ type PermissionsContextProp = {
   checkLocationPermission: () => void;
 };
 
-export const PermissionContext = createContext({} as PermissionsContextProp); // TODO   que exporta el ocntexto hacía afuera
+export const PermissionContext = createContext({} as PermissionsContextProp); //  que exporta el contexto hacía afuera
 
-// { children }:{ children: JSX.Element[]}
+// { children }:{ children: JSX.Element | []}
 export const PermissionProvider = ({ children }: any) => {
   const [permissions, setPermissions] = useState(permissionInitState);
 
-  const askLocationPermission = () => {};
+  const askLocationPermission = async () => {
+    let permissionStatus: PermissionStatus;
+
+    if (Platform.OS === 'ios') {
+      permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    } else {
+      permissionStatus = await request(
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+      );
+    }
+
+    setPermissions({ ...permissions, locationStatus: permissionStatus });
+  };
   const checkLocationPermission = () => {};
 
   return (
